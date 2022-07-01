18e5b590dd5151087b2b077d6cd7bd29b82d0198
ricquiros <r.kiros1@gmail.com>
2022-07-01 13:46:15 +0200

    NICsv2

--- terraform/recursos.tf
+++ terraform/recursos.tf
@@ -48,8 +48,9 @@ resource "azurerm_network_interface" "nicW" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.PublicIp.id
   }
+}
 
-}resource "azurerm_network_interface" "nicN" {
+resource "azurerm_network_interface" "nicN" {
   name                = "vnicN"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
